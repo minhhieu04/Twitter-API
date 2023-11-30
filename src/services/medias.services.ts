d3979e78e8cacf925175f5e4fd988fb36ce7c544
minhhieu04 <99400715+minhhieu04@users.noreply.github.com>
2023-12-01 00:26:27 +0700

    Handle parameter passed from command

--- src/services/medias.services.ts
+++ src/services/medias.services.ts
@@ -3,18 +3,26 @@ import { getFileNameWithoutExtension, handleUploadSingleImage } from '~/utils/fi
 import sharp from 'sharp'
 import { UPLOAD_DIR } from '~/constants/dir'
 import fs from 'fs'
+import { isProduction } from '~/constants/configServer'
+import path from 'path'
 
 class MediaService {
   async handleUploadSingleImage(req: Request) {
     const file = await handleUploadSingleImage(req)
     const newFilename = getFileNameWithoutExtension(file.newFilename)
-    const newPath = UPLOAD_DIR + '/' + newFilename + '.jpg'
+    // const newPath = UPLOAD_DIR + '/' + newFilename + '.jpg'
+    const newPath = path.resolve(UPLOAD_DIR, `${newFilename}.jpg`)
     // Handle rename extension and reduce image quality
     await sharp(file.filepath).jpeg({ quality: 80 }).toFile(newPath)
     // Delete original file in temp folder
-    fs.unlinkSync(file.filepath)
-    // return `${process.env.SERVER_LOCAL_URL} /${newFilename}`
-    return newPath
+    fs.unlink(file.filepath, (err) => {
+      if (err) {
+        console.log(`${file.filepath} was deleted`, err)
+      }
+    })
+    return isProduction
+      ? `${process.env.SERVER_HOST_URL}/medias/photo/${newFilename}`
+      : `${process.env.SERVER_LOCAL_URL}/medias/photo/${newFilename}`
   }
 }
 
