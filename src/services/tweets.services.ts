1141abd35c8705a9b0a0126129b9679cfe62b2ed
Emma minhhieu04 <minhhieu.tran.mcs@gmail.com>
2023-12-18 02:34:00 +0700

    Create hashtags

--- src/services/tweets.services.ts
+++ src/services/tweets.services.ts
@@ -1,15 +1,37 @@
+import Hashtag from '~/models/schemas/Hashtag.schema'
 import Tweet from '~/models/schemas/Tweet.schema'
 import databaseService from './database.services'
 import { TweetReqBody } from '~/models/requests/Tweet.requests'
 import { ObjectId } from 'mongodb'
 
 class TweetsService {
+  async checkAndCreateHashtags(hashtags: string[]) {
+    const hashtagDocuments = await Promise.all(
+      hashtags.map((hashtag) => {
+        // Find the hashtag in the database, if found, take it, if not, create a new one
+        return databaseService.hashtags.findOneAndUpdate(
+          { name: hashtag },
+          {
+            $setOnInsert: new Hashtag({ name: hashtag })
+          },
+          {
+            upsert: true,
+            returnDocument: 'after'
+          }
+        )
+      })
+    )
+    return hashtagDocuments.map((hashtag) => hashtag?._id) as ObjectId[]
+  }
+
   async createTweet(userId: string, body: TweetReqBody) {
+    const hashtags = await this.checkAndCreateHashtags(body.hashtags)
+    console.log(hashtags)
     const result = await databaseService.tweets.insertOne(
       new Tweet({
         audience: body.audience,
         content: body.content,
-        hashtags: [], // TODO: Temporarily leave the array empty
+        hashtags,
         medias: body.medias,
         mentions: body.mentions,
         parent_id: body.parent_id,
